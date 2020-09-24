952ffcf11a6641b7492e19b49444c00df0d611c1
OneCodeMonkey <1460018362@qq.com>
2020-09-24 18:08:19 +0800

    feat: pre-step

---
+++ hyperf_service/test.sh
@@ -0,0 +1,11 @@
+echo "code format"
+ssh vagrant@192.168.10.10 "cd /home/vagrant/Code/content_service && ./vendor/squizlabs/php_codesniffer/bin/phpcs --standard=PSR2 --error-severity=1 --warning-severity=6 app/"
+echo "code format finished!"
+
+echo "static-debugging"
+ssh vagrant@192.168.10.10 "cd /home/vagrant/Code/content_service && ./vendor/phpstan/phpstan/phpstan analyse -l 0 -c phpstan.neon app/"
+echo "static-debugging finished!"
+
+echo "unit-test"
+ssh vagrant@192.168.10.10 "cd /home/vagrant/Code/content_service && ./vendor/bin/phpunit tests/"
+echo "unit-test finished!"
